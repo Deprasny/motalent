1d5759168bc8824cd52bc8795dbfb0c7bb386ba7
deprasny <deprasny@gmail.com>
2023-09-10 19:45:07 +0700

    fix:  add ref to component MotalentInput

--- src/components/shared/molecules/motalent-input.tsx
+++ src/components/shared/molecules/motalent-input.tsx
@@ -1,34 +1,41 @@
-import { useFormField } from "@/components/ui/form";
-import { Input, InputProps } from "@/components/ui/input";
-import { VariantProps, cva } from "class-variance-authority";
+import { useFormField } from '@/components/ui/form';
+import { Input, InputProps } from '@/components/ui/input';
+import { VariantProps, cva } from 'class-variance-authority';
+import React from 'react';
 
-const motalentCVA = cva([""], {
-  variants: {
-    size: {
-      sm: "",
+const motalentCVA = cva([''], {
+    variants: {
+        size: {
+            sm: ''
+        },
+        isError: {
+            true: 'ring-1 ring-red-500 focus-visible:ring-red-500 focus-visible:ring-offset-0'
+        }
     },
-    isError: {
-      true: "ring-1 ring-red-500 focus-visible:ring-red-500 focus-visible:ring-offset-0",
-    },
-  },
-  defaultVariants: {
-    isError: false,
-  },
+    defaultVariants: {
+        isError: false
+    }
 });
 
 type MotalentInputProps = InputProps & VariantProps<typeof motalentCVA>;
 
-export default function MotalentInput(props: MotalentInputProps) {
-  const { id, invalid } = useFormField();
+const MotalentInput = React.forwardRef<HTMLInputElement, MotalentInputProps>(
+    (props, ref) => {
+        const { id, invalid } = useFormField();
 
-  return (
-    <Input
-      id={id}
-      className={motalentCVA({
-        size: props.size,
-        isError: props.isError || invalid,
-      })}
-      {...props}
-    />
-  );
-}
+        return (
+            <Input
+                id={id}
+                className={motalentCVA({
+                    size: props.size,
+                    isError: props.isError || invalid
+                })}
+                ref={ref}
+                {...props}
+            />
+        );
+    }
+);
+
+MotalentInput.displayName = 'Motalent Input';
+export default MotalentInput;
